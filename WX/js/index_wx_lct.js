d940fa3ae4153762b94e5610bda3e48e82db19d9
wo2095296 <372425159@qq.com>
2016-11-16 22:25:43 +0800

    kbtx

---
+++ WX/js/index_wx_lct.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper('.swiper-container1', {
+    pagination: '.swiper-pagination',
+    spaceBetween: 0,
+    centeredSlides: true,
+    autoplay: 3000,
+    effect: 'slide',
+    loop: true,
+    autoplayDisableOnInteraction: false
+});
+var swiper = new Swiper('.swiper-container2', {
+    pagination: '.swiper-pagination',
+    spaceBetween: 0,
+    centeredSlides: true,
+    autoplay: 4500,
+    effect: 'slide',
+    loop: true,
+    autoplayDisableOnInteraction: false
+});
+var swiper = new Swiper('.swiper-container3', {
+    pagination: '.swiper-pagination',
+    spaceBetween: 0,
+    centeredSlides: true,
+    autoplay: 6500,
+    effect: 'slide',
+    loop: true,
+    autoplayDisableOnInteraction: false
+});
